c1310f6cacb4319aaaeb620662eab136ed08d0d2
王政舜 <shun.wang@inspire-dt.com>
2025-08-27 11:49:45 +0800

    feat: add vite multi-page build config and backup products page

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     root: '.',
     publicDir: 'public',
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(process.cwd(), 'index.html'),
+                faq: resolve(process.cwd(), 'FAQ.html'),
+                collections: resolve(process.cwd(), 'collections.html'),
+                contact: resolve(process.cwd(), 'contact.html'),
+                eCatalog: resolve(process.cwd(), 'e-catalog.html'),
+                jobReference: resolve(process.cwd(), 'job+reference.html'),
+                newsDetail: resolve(process.cwd(), 'news-detail.html'),
+                news: resolve(process.cwd(), 'news.html'),
+                products: resolve(process.cwd(), 'products.html'),
+                productDetail: resolve(process.cwd(), 'product-detail.html'),
+            }
+        }
     },
 });
